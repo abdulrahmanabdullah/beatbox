fa96eab36d2aed48a2bd2127f770917b364a40f4
abdulrahmanabdullah <nfs056@gmail.com>
2018-09-08 00:39:37 +0300

    upload project

---
+++ app/src/main/java/com/example/abdulrahman/beatboxdem2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.abdulrahman.beatboxdem2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity  {
+
+    @Override
+    protected Fragment createFragment() {
+        return TestFragment.newInstance();
+    }
+}
